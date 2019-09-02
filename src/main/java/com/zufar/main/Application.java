b003fd156df30c8c25b57481f37bb97aafb6b113
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-09-02 11:06:00 +0400

    Initial commit.

--- src/main/java/com/zufar/main/Application.java
+++ src/main/java/com/zufar/main/Application.java
@@ -1,4 +1,12 @@
 package com.zufar.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
