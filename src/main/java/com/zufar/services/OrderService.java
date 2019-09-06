553a965be22bc150032a62e7326b1b57ae67d5ea
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-09-06 17:01:09 +0400

    ExceptionHandling and logging were added to OrderService.

--- src/main/java/com/zufar/services/OrderService.java
+++ src/main/java/com/zufar/services/OrderService.java
@@ -3,6 +3,8 @@ package com.zufar.services;
 import com.zufar.exceptions.OrderNotFoundException;
 import com.zufar.models.Order;
 import com.zufar.repositories.OrderRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,7 +14,8 @@ import java.util.Collection;
 @Service
 @Transactional(readOnly = true)
 public class OrderService {
-    
+
+    private static final Logger LOGGER = LogManager.getLogger(OrderService.class);
     private OrderRepository orderRepository;
 
     @Autowired
@@ -21,8 +24,12 @@ public class OrderService {
     }
 
     public Order getOrder(Long orderId) throws OrderNotFoundException {
-        return this.orderRepository
-                .findById(orderId).orElseThrow(() -> new OrderNotFoundException("An order with id = " + orderId));
+        return this.orderRepository.findById(orderId).orElseThrow(() -> {
+            final String errorMessage = "Updating an order is impossible. The order with id=" + orderId + " is not found";
+            OrderNotFoundException exception = new OrderNotFoundException(errorMessage);
+            LOGGER.error(errorMessage, exception);
+            return exception;
+        });
     }
 
     public Collection<Order> getOrders() {
@@ -31,21 +38,37 @@ public class OrderService {
     
     public void deleteOrder(Long orderId) throws OrderNotFoundException {
         if (!this.orderRepository.existsById(orderId)) {
-            throw new OrderNotFoundException("An order with id = " + orderId);
+            final String errorMessage = "Deleting an order is impossible. The order with id=" + orderId + " is not found";
+            OrderNotFoundException exception = new OrderNotFoundException(errorMessage);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
         }
         this.orderRepository.deleteById(orderId);
     }
 
     public Order saveOrder(Order order) {
+        if (order == null) {
+            final String errorMessage = "Saving an order is impossible. An order is absent.";
+            IllegalArgumentException exception = new IllegalArgumentException(errorMessage);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
+        }
         return this.orderRepository.save(order);
     }
 
     public Order updateOrder(Order order) throws OrderNotFoundException {
         if (order == null || order.getId() == null) {
-            throw new IllegalArgumentException();
+            final String errorMessage = "Updating an order is impossible. An orderId is absent.";
+            IllegalArgumentException exception = new IllegalArgumentException(errorMessage);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
         }
-        if (!this.orderRepository.existsById(order.getId())) {
-            throw new OrderNotFoundException("An order with id = " + order.getId());
+        final Long orderId = order.getId();
+        if (!this.orderRepository.existsById(orderId)) {
+            final String errorMessage = "Updating an order is impossible. The order with id=" + orderId + " is not found";
+            OrderNotFoundException exception = new OrderNotFoundException(errorMessage);
+            LOGGER.error(errorMessage, exception);
+            throw exception;
         }
         return this.orderRepository.save(order);
     }
