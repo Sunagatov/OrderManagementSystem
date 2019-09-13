074f142ea6d83867db6312a2d3772b5c69a05201
Сунагатов Зуфар Рустамович <sunagatovzr@vdcom.ru>
2019-09-13 15:06:50 +0400

    BCryptPasswordEncoder bean fabric method was added.

--- src/main/java/com/zufar/Application.java
+++ src/main/java/com/zufar/Application.java
@@ -3,11 +3,18 @@ package com.zufar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-       SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public BCryptPasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
